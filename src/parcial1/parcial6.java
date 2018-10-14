63143f19cd0d2bdf58883b9d76ed9241950f6ef8
Nicholl Perez y Santiago Orrego <nichollperezm@outlook.com>
2018-10-13 22:32:19 -0500

    Este es el parcial 6

---
+++ src/parcial1/parcial6.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcial1;
+
+/**
+ *
+ * @author nicho
+ */
+public class parcial6 {
+    /**
+ *
+ * @author nicholl
+ */
+
+ public static void main (String[] args) {
+     
+        // TODO code application logic here
+        // Definir el vector.
+        char[]alfabeto = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+        int num = 11;
+    
+        //Comenzamos a unir las letras con los numeros(tratamos de hacer las cosas de una manera mas extensa, pero descubrimos que era mas sencillo de esta manera).
+        
+        System.out.println(num+ " --> " +alfabeto[num]);
+    }
+}
